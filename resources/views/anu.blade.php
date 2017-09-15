0974a9e013dbd7ec8f880e64fc54ad1252033e85
Mhari3 <nrmuhamad@gmail.com>
2017-09-15 08:12:54 +0700

    Menambahkan route sebagai pengetesan

---
+++ resources/views/anu.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Testing halaman</title>
+</head>
+<body>
+Hanya testing broo... :)
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/anu', function () {
+    return view('anu');
+});
