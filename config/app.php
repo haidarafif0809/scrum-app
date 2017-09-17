7a6fae67ffe5de78ef2514734377ab71234f8a3d
Muhammad Hari <Nrmuhamad@gmail.com>
2017-09-18 06:07:00 +0700

    Mengubah file app.php
    Mengubah konfigurasi di file app.php

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
         Laratrust\LaratrustServiceProvider::class,
         Yajra\Datatables\DatatablesServiceProvider::class,
 
@@ -236,6 +237,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Laratrust' => Laratrust\LaratrustFacade::class,
 
     ],
 
