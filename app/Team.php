abf36a29922b303c298b2280415856c46192abe5
Galang Cahya Pamungkas <galangcahyapamungkas@gmail.com>
2017-10-06 11:00:54 +0700

    memperbaiki pembatasan penghapusan pada team

--- app/Team.php
+++ app/Team.php
@@ -7,24 +7,18 @@ use Session;
 class Team extends Model
 {
 
-	public static function boot()
-	{
+	
 
-	self::deleting(function($team)
-	{
-		if ($team->user()->count() > 0) {
-				Session::flash("flash_notification", [
-					"level"=>"danger",
-					"message"=> "Team ". $team->nama_team ." masih digunakan."
-				]);
-			return false;
-			}
-		});
-	}
 
         protected $fillable = ['kode_team', 'nama_team'];
 
         public function user() {
-        	return $this->hasMany('App\User');
+        	return $this->hasMany('App\TeamUser');
         }
+
+        public function sprint() {
+        	return $this->hasMany('App\Sprint');
+        }
+
+
 }
