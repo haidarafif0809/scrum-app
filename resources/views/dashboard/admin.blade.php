d59e054550aa9cb3a6f2823044249f8dca13f4a4
Muhammad Khoirul Anwar <mkhoirulanwar41@gmail.com>
2017-10-09 11:38:12 +0700

    Auto stash before merge of "master" and "origin/master"

---
+++ resources/views/dashboard/admin.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h2 class="panel-title">Dashboard</h2>
+					<div class="panel-body">
+						Selamat datang di Menu Aplikasi Scrum Silahkan pilih menu yang diinginkan.
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+@endsection
