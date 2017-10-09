f2d3afb0bf33b434ab247853ab1adbbf546a57e6
Muhammad Khoirul Anwar <mkhoirulanwar41@gmail.com>
2017-10-09 13:15:25 +0700

    Merubah text di admin dashboard

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -8,8 +8,8 @@
 					<div class="panel-heading">
 						<h2 class="panel-title">Dashboard</h2>
 					<div class="panel-body">
-						<h2>Selamat datang di menu aplikasi scrum</h2>
-						<h3>Pilih menu yang anda inginkan.</h3>
+						Selamat datang di menu aplikasi scrum <br>
+							Pilih menu yang anda inginkan
 						</div>
 					</div>
 				</div>
