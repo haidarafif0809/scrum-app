35f92bf78c03c09ca3b8344f836e948fa3a255d6
khofiq <khoirulgeong123@gmail.com>
2017-10-13 11:16:25 +0700

    ubah password

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -3,19 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Validate;
 use Auth;
-
+use Session;
 class SettingsController extends Controller
 {
-    public function editPassword() {
-    	return view('settings.editPassword');
-    }
+	public function editPassword()
+	{
+		return view('settings.editPassword');
+	}
 
-    public function updatePassword() {
-    	$user = Auth::user();
-    	$this->validate($request, [
-    		'password' => 'required|passcheck:'. $user->password
-    	]); 
-    }
+	public function updatePassword(Request $request)
+	{
+		$user = Auth::user();
+		$this->validate($request, [
+			'password' => 'required|passcheck:' . $user->password,
+			'new_password' => 'required|confirmed|min:6',
+			], [
+					'password.passcheck' => 'Password lama tidak sesuai'
+				]);
+		$user->password = $request->get('new_password');
+		$user->save();
+		Session::flash("flash_notification", [
+			"level"=>"success",
+			"message"=>"Password berhasil diubah"
+		]);
+		return redirect('settings/password');
+	}
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Validator;
+use Hash;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('passcheck', function ($attribute, $value, $parameters) {
+        return Hash::check($value, $parameters[0]); });
     }
 
     /**
