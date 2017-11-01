4c851c55bc0c3685f9e40c5c2c3937cb4d8fd78c
Muhammad Khoirul Anwar <mkhoirulanwar41@gmail.com>
2017-11-01 10:35:36 +0700

    Menambahkan panel di dashboard member

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,27 +28,36 @@ class HomeController extends Controller
     public function index()
     {   
         if (Laratrust::hasRole('admin')) return $this->adminDashboard();
-         if (Laratrust::hasRole('member')) return $this->memberDashboard();
-        return view('home');
-    }
-    protected function adminDashboard()
-    {   
-        $jumlah_member = RoleUser::where('role_id',2)->count();
-        $jumlah_team = Team::count();
-        $jumlah_sprint = Sprint::count();
-        $jumlah_backlog = Backlog::count();
-        return view('dashboard.admin',[
-            'jumlah_member' => $jumlah_member,
-            'jumlah_team' => $jumlah_team,
-            'jumlah_sprint' => $jumlah_sprint,
-            'jumlah_backlog' => $jumlah_backlog
+           if (Laratrust::hasRole('member')) return $this->memberDashboard();
+            return view('home');
+        }
+        protected function adminDashboard()
+        {   
+            $jumlah_member  = RoleUser::where('role_id',2)->count();
+            $jumlah_team    = Team::count();
+            $jumlah_sprint  = Sprint::count();
+            $jumlah_backlog = Backlog::count();
+            return view('dashboard.admin',[
+                'jumlah_member' => $jumlah_member,
+                'jumlah_team'   => $jumlah_team,
+                'jumlah_sprint' => $jumlah_sprint,
+                'jumlah_backlog'=> $jumlah_backlog
+                
+            ]);
             
-        ]);
-        
-    }
+        }
 
-    protected function memberDashboard()
-    {
-        return view('dashboard.member');
-    }
-}
+        protected function memberDashboard()
+        {
+         $jumlah_member = RoleUser::where('role_id',2)->count();
+         $jumlah_team = Team::count();
+         $jumlah_sprint = Sprint::count();
+         $jumlah_backlog = Backlog::count();
+         return view('dashboard.member', [
+           'jumlah_member'    => $jumlah_member,
+           'jumlah_team'      => $jumlah_team,
+           'jumlah_sprint'    => $jumlah_sprint,
+           'jumlah_backlog'   => $jumlah_backlog
+       ]);
+     }
+ }
--- resources/views/dashboard/member.blade.php
+++ resources/views/dashboard/member.blade.php
@@ -1,19 +1,66 @@
 @extends('layouts.app')
 
-@section('content')
-	<div class="container">
-		<div class="row">
-			<div class="col-md-10 col-md-offset-1">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h2 class="panel-title">Dashboard</h2>
-					</div>
+@section('title', 'Dasboard Member')
 
-					<div class="panel-body">
-						Selamat datang di Aplikasi Scrum
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="panel panel-primary">
+                <div class="panel-heading"><span class="glyphicon glyphicon-retweet"></span>
+                    <strong>Backlog</strong>
+                </div>
+                <div class="panel-body">
+                    <strong>
+                        Jumlah Backlog {{ $jumlah_backlog }}  
+                    </strong>           
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-md-3">
+            <div class="panel panel-danger">
+                <div class="panel-heading"><span class="glyphicon glyphicon-credit-card"></span>
+                    <strong>Team</strong>
+                </div>
+                <div class="panel-body">
+                    <strong>
+                     Jumlah Team {{ $jumlah_team }}
+                 </strong>
+             </div>
+         </div>
+     </div>
+
+
+
+     <div class="col-md-3">
+        <div class="panel panel-success">
+            <div class="panel-heading"><span class="glyphicon glyphicon-object-align-left"></span>
+                <strong>Sprint</strong>
+            </div>
+            <div class="panel-body">
+                <strong>
+                    Jumlah Sprint {{ $jumlah_sprint }}
+                </strong>
+            </div>
+        </div>
+    </div>
+
+    
+    <div class="col-md-3">
+        <div class="panel panel-warning">
+            <div class="panel-heading"><span class="glyphicon glyphicon-edit"></span>
+                <strong>Member</strong>
+            </div>
+            <div class="panel-body">
+                <strong>
+                    Jumlah Member {{ $jumlah_member }}
+                </strong>
+            </div>
+        </div>
+    </div>
+
+</div>
+</div>
 @endsection
