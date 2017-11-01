0bc630517bf3d02f260d5fe6afd65ff38700fe38
Muhammad Hari <Nrmuhamad@gmail.com>
2017-11-01 10:30:34 +0700

    Menginstal CKeditor pada input catatan di halaman tambah Backlog

--- resources/views/backlog/_form.blade.php
+++ resources/views/backlog/_form.blade.php
@@ -2,34 +2,35 @@
 	{!! Form::label('aplikasi_id', 'Aplikasi', ['class'=>'col-md-2 control-label']) !!}
 	<div class="col-md-4">
 		<!-- {!! Form::text('aplikasi', null, ['class'=>'form-control']) !!}
-		{!! $errors->first('aplikasi', '<p class="help-block">:message</p>') !!} -->
-		{!! Form::select('aplikasi_id', [''=>'']+App\Aplication::pluck('nama','id')->all(), null, ['class' => 'form-control js-selectize-reguler', 'placeholder' => 'Pilih Aplikasi']) !!}
-		{!! $errors->first('author_id', '<p class="help-block">:message</p>') !!}
+			{!! $errors->first('aplikasi', '<p class="help-block">:message</p>') !!} -->
+			{!! Form::select('aplikasi_id', [''=>'']+App\Aplication::pluck('nama','id')->all(), null, ['class' => 'form-control js-selectize-reguler', 'placeholder' => 'Pilih Aplikasi']) !!}
+			{!! $errors->first('author_id', '<p class="help-block">:message</p>') !!}
+		</div>
 	</div>
-</div>
-<div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-	{!! Form::label('nama_backlog', 'Nama Backlog', ['class'=>'col-md-2 control-label']) !!}
-	<div class="col-md-4">
-		{!! Form::text('nama_backlog', null, ['class'=>'form-control']) !!}
-		{!! $errors->first('nama', '<p class="help-block">:message</p>') !!}
+	<div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+		{!! Form::label('nama_backlog', 'Nama Backlog', ['class'=>'col-md-2 control-label']) !!}
+		<div class="col-md-4">
+			{!! Form::text('nama_backlog', null, ['class'=>'form-control']) !!}
+			{!! $errors->first('nama', '<p class="help-block">:message</p>') !!}
+		</div>
 	</div>
-</div>
-<div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-	{!! Form::label('demo', 'Demo', ['class'=>'col-md-2 control-label']) !!}
-	<div class="col-md-4">
-		{!! Form::textarea('demo', null, ['class'=>'form-control', 'size' => '20x2']) !!}
-		{!! $errors->first('demo', '<p class="help-block">:message</p>') !!}
+	<div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+		{!! Form::label('demo', 'Demo', ['class'=>'col-md-2 control-label']) !!}
+		<div class="col-md-4">
+			{!! Form::textarea('demo', null, ['class'=>'form-control', 'size' => '20x2']) !!}
+			{!! $errors->first('demo', '<p class="help-block">:message</p>') !!}
+		</div>
 	</div>
-</div>
-<div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-	{!! Form::label('catatan', 'Catatan', ['class'=>'col-md-2 control-label']) !!}
-	<div class="col-md-4">
-		{!! Form::textarea('catatan', null, ['class'=>'form-control', 'size' => '20x2']) !!}
-		{!! $errors->first('catatan', '<p class="help-block">:message</p>') !!}
+	<div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+		{!! Form::label('catatan', 'Catatan', ['class'=>'col-md-2 control-label']) !!}
+		<div class="col-md-10">
+			{!! Form::textarea('catatan', null, ['class'=>'form-control', 'size' => '20x2', 'id' => 'editorCatatan']) !!}
+			{!! $errors->first('catatan', '<p class="help-block">:message</p>') !!}
+
+		</div>
 	</div>
-</div>
-<div class="form-group">
-	<div class="col-md-4 col-md-offset-2">
-		{!! Form::submit('Simpan', ['class'=>'btn btn-primary']) !!}
+	<div class="form-group">
+		<div class="col-md-4 col-md-offset-2">
+			{!! Form::submit('Simpan', ['class'=>'btn btn-primary']) !!}
+		</div>
 	</div>
-</div>
