1318a9a8950e6e359c10d9fac4320a9e4755c11c
ahyadii <ranudinata101@gmail.com>
2017-11-01 17:03:26 +0700

    memperbaiki view show pada sprintbacklog

--- app/Http/Controllers/SprintbacklogsController.php
+++ app/Http/Controllers/SprintbacklogsController.php
@@ -38,7 +38,7 @@ class SprintbacklogsController extends Controller
         ->addColumn(['data' => 'perkiraan_waktu', 'name'=>'perkiraan_waktu', 'title'=>'Perkiraan Waktu']) 
         ->addColumn(['data' => 'action', 'name'=>'action', 'title'=>'Aksi', 'orderable'=>false, 'searchable'=>false]); 
         
-        return view('Sprintbacklogs.show')->with(compact('html')); 
+        return view('sprintbacklogs.show')->with(compact('html')); 
     } 
     
     /** 
@@ -140,7 +140,7 @@ class SprintbacklogsController extends Controller
         ->addColumn(['data' => 'assign', 'name'=>'assign', 'title'=>'Assign', 'orderable'=>false, 'searchable'=>false])
         ->addColumn(['data' => 'action', 'name'=>'action', 'title'=>'Aksi', 'orderable'=>false, 'searchable'=>false]);
         
-        return view('Sprintbacklogs.show',['sprint'=>$id])->with(compact('html')); 
+        return view('sprintbacklogs.show',['sprint'=>$id])->with(compact('html')); 
     } 
 
     public function edit($id) 
@@ -192,7 +192,7 @@ class SprintbacklogsController extends Controller
 
      public function assign($id) 
     { 
-        
+
     } 
     
         public function export($id) {
