f10a1b8fcda32c09eb7d49907420fe8c74d230cd
khofiq <khoirulgeong123@gmail.com>
2017-11-10 10:37:45 +0700

    menambah assign dan finish di user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('jml_assign')->nullable();
+            $table->integer('jml_finish')->nullable();
             $table->boolean('is_verified')->default(0);
             $table->integer('created_by')->nullable()->index();
             $table->integer('updated_by')->nullable()->index();
