ff684cceab1308d0acbff5bfc16de2834941da17
Muhammad Khoirul Anwar <mkhoirulanwar41@gmail.com>
2017-11-14 10:34:09 +0700

    Memperbaiki dry di homecontroller.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,44 +28,30 @@ class HomeController extends Controller
      */
     public function index()
     {   
-        if (Laratrust::hasRole('admin')) return $this->adminDashboard();
-           if (Laratrust::hasRole('member')) return $this->memberDashboard();
-            return view('home');
-        }
-        protected function adminDashboard()
-        {   
-            $jumlah_member  = RoleUser::where('role_id',2)->count();
-            $jumlah_team    = Team::count();
-            $jumlah_sprint  = Sprint::count();
-            $jumlah_backlog = Backlog::count();
-            $jumlah_assign = Sprintbacklog::where('assign',1)->count();
-            $jumlah_finish = Sprintbacklog::where('finish',1)->count();
-            return view('dashboard.admin',[
-                'jumlah_member' => $jumlah_member,
-                'jumlah_team'   => $jumlah_team,
-                'jumlah_sprint' => $jumlah_sprint,
-                'jumlah_backlog'=> $jumlah_backlog,
-                'jumlah_assign' => $jumlah_assign,
-                'jumlah_finish' => $jumlah_finish
-            ]);
-            
-        }
-
-        protected function memberDashboard()
-        {
-           $jumlah_member  = RoleUser::where('role_id',2)->count();
-           $jumlah_team    = Team::count();
-           $jumlah_sprint  = Sprint::count();
-           $jumlah_backlog = Backlog::count();
-           $jumlah_assign = Sprintbacklog::where('assign',1)->count();
-           $jumlah_finish = Sprintbacklog::where('finish',1)->count();
-           return view('dashboard.admin',[
-            'jumlah_member' => $jumlah_member,
-            'jumlah_team'   => $jumlah_team,
-            'jumlah_sprint' => $jumlah_sprint,
-            'jumlah_backlog'=> $jumlah_backlog,
-            'jumlah_assign' => $jumlah_assign,
-            'jumlah_finish' => $jumlah_finish
-        ]);
-       }
+       $jumlah_member  = RoleUser::where('role_id',2)->count();
+       $jumlah_team    = Team::count();
+       $jumlah_sprint  = Sprint::count();
+       $jumlah_backlog = Backlog::count();
+       $jumlah_assign = Sprintbacklog::where('assign',1)->count();
+       $jumlah_finish = Sprintbacklog::where('finish',1)->count();
+       return view('dashboard.admin',[
+        'jumlah_member' => $jumlah_member,
+        'jumlah_team'   => $jumlah_team,
+        'jumlah_sprint' => $jumlah_sprint,
+        'jumlah_backlog'=> $jumlah_backlog,
+        'jumlah_assign' => $jumlah_assign,
+        'jumlah_finish' => $jumlah_finish
+    ]);
+       
    }
+   protected function adminDashboard()
+   {   
+
+   }
+
+   protected function memberDashboard()
+   {
+      
+    
+   }
+}
